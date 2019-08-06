79014fb5463f74f0dfabf5466f4a21f963aea188
Saad Afaan Syed <saadafaan.syed@mail.utoronto.ca>
2019-08-06 14:17:05 -0400

    updated PlayChess. figured out how to set images through java. Need to figure out how to get the location(x and y coordinates of layouts and imageviews

--- app/src/main/java/com/example/magicchess/PlayChess.java
+++ app/src/main/java/com/example/magicchess/PlayChess.java
@@ -9,13 +9,16 @@ import android.graphics.Point;
 import android.graphics.Rect;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.AbsoluteLayout;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
 import com.example.magicchess.engine.board.BoardUtils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class PlayChess extends AppCompatActivity {
 
@@ -27,38 +30,93 @@ public class PlayChess extends AppCompatActivity {
         setContentView(R.layout.activity_play_chess);
 
         List<ImageView> tiles = new ArrayList<>();
-        for (int i = 0; i <= 64; i++) {
-            tiles.add((ImageView) findViewById(R.id.a8));
-        }
-        /*//ImageView view = (ImageView) getResources().getDrawable(R.drawable.rook);
-        Drawable myDrawable = getResources().getDrawable(R.drawable.rook);
-        //myDrawable.setImageResource(R.drawable.myDrawable);
-        ImageView view = (ImageView) myDrawable;*/
-        /*int[] outLocation = new int[2];
-        tiles.get(0).getLocationOnScreen(outLocation);
-        //ImageView tile1 = findViewById(R.id.a8);
-        ImageView imageView = null;
-        imageView.setImageResource(R.drawable.rook);
-        imageView.setX(outLocation[0]);
-        imageView.setX(outLocation[1]);*/
+
+        tiles.add((ImageView) findViewById(R.id.a8));
+        tiles.add((ImageView) findViewById(R.id.b8));
+        tiles.add((ImageView) findViewById(R.id.c8));
+        tiles.add((ImageView) findViewById(R.id.d8));
+        tiles.add((ImageView) findViewById(R.id.e8));
+        tiles.add((ImageView) findViewById(R.id.f8));
+        tiles.add((ImageView) findViewById(R.id.g8));
+        tiles.add((ImageView) findViewById(R.id.h8));
+        tiles.add((ImageView) findViewById(R.id.a7));
+        tiles.add((ImageView) findViewById(R.id.b7));
+        tiles.add((ImageView) findViewById(R.id.c7));
+        tiles.add((ImageView) findViewById(R.id.d7));
+        tiles.add((ImageView) findViewById(R.id.e7));
+        tiles.add((ImageView) findViewById(R.id.f7));
+        tiles.add((ImageView) findViewById(R.id.g7));
+        tiles.add((ImageView) findViewById(R.id.h7));
+        tiles.add((ImageView) findViewById(R.id.a6));
+        tiles.add((ImageView) findViewById(R.id.b6));
+        tiles.add((ImageView) findViewById(R.id.c6));
+        tiles.add((ImageView) findViewById(R.id.d6));
+        tiles.add((ImageView) findViewById(R.id.e6));
+        tiles.add((ImageView) findViewById(R.id.f6));
+        tiles.add((ImageView) findViewById(R.id.g6));
+        tiles.add((ImageView) findViewById(R.id.h6));
+        tiles.add((ImageView) findViewById(R.id.a5));
+        tiles.add((ImageView) findViewById(R.id.b5));
+        tiles.add((ImageView) findViewById(R.id.c5));
+        tiles.add((ImageView) findViewById(R.id.d5));
+        tiles.add((ImageView) findViewById(R.id.e5));
+        tiles.add((ImageView) findViewById(R.id.f5));
+        tiles.add((ImageView) findViewById(R.id.g5));
+        tiles.add((ImageView) findViewById(R.id.h5));
+        tiles.add((ImageView) findViewById(R.id.a4));
+        tiles.add((ImageView) findViewById(R.id.b4));
+        tiles.add((ImageView) findViewById(R.id.c4));
+        tiles.add((ImageView) findViewById(R.id.d4));
+        tiles.add((ImageView) findViewById(R.id.e4));
+        tiles.add((ImageView) findViewById(R.id.f4));
+        tiles.add((ImageView) findViewById(R.id.g4));
+        tiles.add((ImageView) findViewById(R.id.h4));
+        tiles.add((ImageView) findViewById(R.id.a3));
+        tiles.add((ImageView) findViewById(R.id.b3));
+        tiles.add((ImageView) findViewById(R.id.c3));
+        tiles.add((ImageView) findViewById(R.id.d3));
+        tiles.add((ImageView) findViewById(R.id.e3));
+        tiles.add((ImageView) findViewById(R.id.f3));
+        tiles.add((ImageView) findViewById(R.id.g3));
+        tiles.add((ImageView) findViewById(R.id.h3));
+        tiles.add((ImageView) findViewById(R.id.a2));
+        tiles.add((ImageView) findViewById(R.id.b2));
+        tiles.add((ImageView) findViewById(R.id.c2));
+        tiles.add((ImageView) findViewById(R.id.d2));
+        tiles.add((ImageView) findViewById(R.id.e2));
+        tiles.add((ImageView) findViewById(R.id.f2));
+        tiles.add((ImageView) findViewById(R.id.g2));
+        tiles.add((ImageView) findViewById(R.id.h2));
+        tiles.add((ImageView) findViewById(R.id.a1));
+        tiles.add((ImageView) findViewById(R.id.b1));
+        tiles.add((ImageView) findViewById(R.id.c1));
+        tiles.add((ImageView) findViewById(R.id.d1));
+        tiles.add((ImageView) findViewById(R.id.e1));
+        tiles.add((ImageView) findViewById(R.id.f1));
+        tiles.add((ImageView) findViewById(R.id.g1));
+        tiles.add((ImageView) findViewById(R.id.h1));
+
 
         ImageView imageView = new ImageView(this);
         imageView.setImageResource(R.drawable.rook);
         RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.RelativeLayout01);
 
-        int[] outLocation = new int[2];
-        tiles.get(0).getLocationOnScreen(outLocation);
-        relativeLayout.setX(outLocation[0]);
-        relativeLayout.setY(outLocation[1]);
-
         RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
                 RelativeLayout.LayoutParams.WRAP_CONTENT,
                 RelativeLayout.LayoutParams.WRAP_CONTENT
         );
-        //layoutParams.addRule(RelativeLayout.BELOW, R.id.buttonCalculate);
-        //layoutParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
+
+        int tileX = (int) tiles.get(0).getX();
+        int tileY = (int) tiles.get(0).getY();
+        System.out.println(tileX);
+
+        relativeLayout.setX(100);
+        relativeLayout.setY(100);
 
         relativeLayout.addView(imageView, layoutParams);
+        imageView.setLayoutParams(layoutParams);
+
+        RelativeLayout gridLayout = (RelativeLayout) findViewById(R.id.gridLayout);
 
 
     }
